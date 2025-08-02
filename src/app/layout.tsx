ebfc0d5e5960829bc399c9bcdda8226836eebffd
Jackie Jensen <jackiehartvig@gmail.com>
2025-08-02 20:09:37 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,9 +46,9 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
@@ -76,6 +76,14 @@ export default function RootLayout({
           src={`https://fundingchoicesmessages.google.com/i/pub-9894760850635221?ers=1`}
         />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9894760850635221" crossOrigin="anonymous"></script>
+        <script type="application/ld+json">
+          {`{
+            "@context": "https://schema.org",
+            "@type": "Organization",
+            "url": "https://jsontomodel.com",
+            "logo": "https://jsontomodel.com/favicon.ico"
+          }`}
+        </script>
       </head>
       <body className="font-body antialiased">
         <ThemeProvider
